94121e90d730a9a211401eb0028cd5cde5e5a92d
Nestor hdez <nestorhdez.26@gmail.com>
2019-10-21 15:16:03 +0100

    Form component reusable on Edit and Add tour.

--- src/views/AddTour.vue
+++ src/views/AddTour.vue
@@ -2,29 +2,14 @@
     <div id="create-tour">
         <h1>Create a new tour</h1>
         <span id="error" v-if="error.status">{{error.msg}}</span>
-        <div id="create-container">
-            <div class="form-section">
-                <label for="title">Title*</label>
-                <input v-model="tour.name" type="text" id="title" placeholder="Title of the tour" required/>
-            </div>
-
-            <div class="form-section">
-                <label for="image">Image</label>
-                <input @change="changeFile" ref="file" type="file" id="image"/>
-            </div>
-
-            <div class="form-section">
-                <label for="description">Description*</label>
-                <textarea v-model="tour.description" rows="10" id="description" placeholder="Description of the tour" required/>
-            </div>
-
-            <button @click="createTour">Create</button>
-
-        </div>
+        <Form @create="createTour" :tour="tour" btnName="Create"/>
     </div>
 </template>
 
 <script>
+
+    import Form from '../components/TourForm';
+
     export default {
         data() {
             return {
@@ -40,10 +25,6 @@
             };
         },
         methods: {
-            changeFile() {
-                this.tour.image = this.$refs.file.files[0];
-                console.log(this.$refs.file.files[0]);
-            },
             createTour() {
                 this.error.status = false;
                 if (this.tour.name && this.tour.description) {
@@ -59,6 +40,9 @@
                 }
             },
         },
+        components: {
+            Form,
+        }
     };
 </script>
 
@@ -70,75 +54,6 @@
 
     h1 {
         margin-bottom: 20px;
-    }
-
-    #create-container {
-        background-color: #fff;
-        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
-        border-radius: 3px;
-        text-align: left;
-        padding: 20px 50px;
-        width: 80%;
-        margin: 0 auto;
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    .form-section:nth-child(1) {
-        flex-basis: 60%;
-    }
-
-    .form-section:nth-child(2) {
-        flex-basis: 30%;
-        margin-left: auto;
-    }
-
-    .form-section:nth-child(3) {
-        flex-basis: 100%;
-    }
-
-    label {
-        display: block;
-        margin: 15px 0;
-        font-weight: 500;
-    }
-
-    input {
-        outline: none;
-    }
-
-    #title {
-        width: 100%;
-        padding: 8px;
-        border-radius: 3px;
-        border: 1px solid #bdbdbd;
-        font-size: 1rem;
-    }
-
-    textarea {
-        width: 100%;
-        outline: none;
-        border-radius: 3px;
-        border: 1px solid #bdbdbd;
-        padding: 20px;
-        font-size: 1rem;
-    }
-
-    button {
-        margin-top: 20px;
-        padding: 5px 10px;
-        border-radius: 3px;
-        background-color: #35495d;
-        color: white;
-        font-size: 0.9rem;
-        font-weight: 500;
-        margin-left: auto;
-        outline: none;
-        border: none;
-    }
-
-    button:active {
-        background-color: #232d38;
     }
 
     #error {
@@ -153,14 +68,6 @@
 
         #create-tour {
             padding: 40px 20px;
-        }
-
-        #create-container {
-            width: 100%;
-        }
-
-        .form-section:nth-child(1) {
-            flex-basis: 100%;
         }
 
     }
--- src/views/EditTour.vue
+++ src/views/EditTour.vue
@@ -1,30 +1,15 @@
 <template>
-    <div id="create-tour">
+    <div id="edit-tour">
         <h1>Edit tour</h1>
         <span id="message" v-if="message.status" :style="message.res ? 'color: #2dad2b;' : ''">{{message.msg}}</span>
-        <div id="edit-container" v-if="tour">
-            <div class="form-section">
-                <label for="title">Title*</label>
-                <input v-model="tour.name" type="text" id="title" placeholder="Title of the tour" required/>
-            </div>
-
-            <div class="form-section">
-                <label for="image">Image</label>
-                <input @change="changeFile" ref="file" type="file" id="image"/>
-            </div>
-
-            <div class="form-section">
-                <label for="description">Description*</label>
-                <textarea v-model="tour.description" rows="10" id="description" placeholder="Description of the tour" required/>
-            </div>
-
-            <button @click="editTour">Create</button>
-
-        </div>
+        <Form @edit="editTour" :tour="tour" btnName="Edit"/>
     </div>
 </template>
 
 <script>
+
+    import Form from '../components/TourForm';
+
     export default {
         data() {
             return {
@@ -37,10 +22,6 @@
             };
         },
         methods: {
-            changeFile() {
-                this.tour.image = this.$refs.file.files[0];
-                console.log(this.$refs.file.files[0]);
-            },
             editTour() {
                 this.message.status = false;
                 if (this.tour.name && this.tour.description) {
@@ -68,86 +49,20 @@
                 .then((res) => this.tour = res.data)
                 .catch(() => this.$router.go(-1));
         },
+        components: {
+            Form,
+        }
     };
 </script>
 
 <style lang="scss" scoped>
 
-    #create-tour {
+    #edit-tour {
         padding: 40px;
     }
 
     h1 {
         margin-bottom: 20px;
-    }
-
-    #edit-container {
-        background-color: #fff;
-        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
-        border-radius: 3px;
-        text-align: left;
-        padding: 20px 50px;
-        width: 80%;
-        margin: 0 auto;
-        display: flex;
-        flex-wrap: wrap;
-    }
-
-    .form-section:nth-child(1) {
-        flex-basis: 60%;
-    }
-
-    .form-section:nth-child(2) {
-        flex-basis: 30%;
-        margin-left: auto;
-    }
-
-    .form-section:nth-child(3) {
-        flex-basis: 100%;
-    }
-
-    label {
-        display: block;
-        margin: 15px 0;
-        font-weight: 500;
-    }
-
-    input {
-        outline: none;
-    }
-
-    #title {
-        width: 100%;
-        padding: 8px;
-        border-radius: 3px;
-        border: 1px solid #bdbdbd;
-        font-size: 1rem;
-    }
-
-    textarea {
-        width: 100%;
-        outline: none;
-        border-radius: 3px;
-        border: 1px solid #bdbdbd;
-        padding: 20px;
-        font-size: 1rem;
-    }
-
-    button {
-        margin-top: 20px;
-        padding: 5px 10px;
-        border-radius: 3px;
-        background-color: #35495d;
-        color: white;
-        font-size: 0.9rem;
-        font-weight: 500;
-        margin-left: auto;
-        outline: none;
-        border: none;
-    }
-
-    button:active {
-        background-color: #232d38;
     }
 
     #message {
@@ -160,16 +75,8 @@
 
     @media (max-width: 768px) {
 
-        #create-tour {
+        #edit-tour {
             padding: 40px 20px;
-        }
-
-        #edit-container {
-            width: 100%;
-        }
-
-        .form-section:nth-child(1) {
-            flex-basis: 100%;
         }
 
     }
